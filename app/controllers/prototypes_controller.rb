86069e1e84730e32ea7f87bbaeb9a2d84905e7a6
ASHINA3 <upa_shop@yahoo.co.jp>
2021-09-02 12:08:58 +0900

    ストロングパラメーターの設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -4,10 +4,15 @@ class PrototypesController < ApplicationController
   end
 
   def new
-    
+    @prototype = Prototype.new
   end
 
   def create
-    
+    Prototype.create(prototype_params)
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image, :user).merge(user_id: current_user.id)
   end
 end
