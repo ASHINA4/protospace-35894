355c25bacac6438a8c4bf4506f70084b0dd3bf4f
ASHINA3 <upa_shop@yahoo.co.jp>
2021-09-03 12:58:56 +0900

    投稿編集機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -21,6 +21,21 @@ class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id])
   end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if 
+      @prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render action: :edit
+    end
+  end
+
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image, :user).merge(user_id: current_user.id)
